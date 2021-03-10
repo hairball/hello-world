d2cac013150c9137c19af4b7ddae6bf24c1bb4c1
chuck <chuck.clelland@gmail.com>
2021-03-10 15:19:39 -0800

    Updated constructor to accept props.

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,8 +2,8 @@ import React, {Component} from "react";
 
 class Message extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             message: 'Welcome Visitor'
         }
